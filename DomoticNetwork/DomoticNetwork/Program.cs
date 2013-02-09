8b310e88fd29ea457e24c701b71c0621886c8a44
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-02-09 18:40:22 +0000

    Module Example Add

--- DomoticNetwork/DomoticNetwork/Program.cs
+++ DomoticNetwork/DomoticNetwork/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DomoticNetwork.Module;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,40 @@ namespace DomoticNetwork
     {
         static void Main(string[] args)
         {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            ////MODULE
+            //ModuleLoader ml = new ModuleLoader();
+
+            ////recorro cada fichero del directorio en el que se encuentra esta aplicacion e intento cargarlo como módulo.
+            //foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory()+"//Modules"))
+            //{
+            //    ml.LoadModule(file);
+            //}
+
+            ////Llamo al método "method" de cada módulo
+            //foreach (IModule m in ml.Modules)
+            //{
+            //    m.method();
+            //}
+
+            ////END MODULE
+
+            Console.WriteLine("End");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ DomoticNetwork/Module1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomoticNetwork.Module;
+
+namespace Module1
+{
+    public class SampleModule2 : IModule
+    {
+
+        public void method()
+        {
+            Console.WriteLine("Hello Module 1 :D Miau miauuuuuu");
+        }
+    }
+}
