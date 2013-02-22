6217804c3bfaa22af080ef5032f41daf07ce592b
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-02-22 12:13:54 +0000

    Add cosas geniales

---
+++ trunk/DomoticNetwork/DomoticNetwork/Plugin/PluginsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomoticNetwork.Plugin
+{
+    /// <summary>
+    /// Mantiene una referencia al plugin como así también información sobre el nombre del assembly que lo contiene y el AppDomain en donde se cargó. Esta clase también es la encargada de invocar al método Execute() del plugin con el mensaje adecuado y debería ser capaz (no lo es en este ejemplo) de controlar el tiempo de ejecución del mismo. Controla también que el Plugin no dispare una excepción y si lo hace simplemente descarga el AppDomain en el que se encuentra el Plugin.
+    /// </summary>
+    class PluginsController
+    {
+    }
+}
---
+++ trunk/DomoticNetwork/Module2/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomoticNetwork.Module;
+
+namespace Module2
+{
+    public class Class1: IModule
+    {
+        public void method()
+        {
+            Console.WriteLine(":D");
+        }
+    }
+}
