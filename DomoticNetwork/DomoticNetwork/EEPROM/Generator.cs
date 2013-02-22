d5c6674f4f87af76d3f5e299368ee53a8c2f9dbb
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-02-22 14:54:15 +0000

    Updates :D

--- DomoticNetwork/DomoticNetwork/EEPROM/Generator.cs
+++ DomoticNetwork/DomoticNetwork/EEPROM/Generator.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DomoticNetwork.NetworkModel;
 using Extensions;
-using DomoticNetwork.NetworkModel;
+using System;
+using System.Collections.Generic;
 
 namespace DomoticNetwork.EEPROM
 {
