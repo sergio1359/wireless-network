403fa5c489e591fd6dc59ceb229261a7a85d6d1b
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-02-22 12:13:54 +0000

    Add cosas geniales

---
+++ DomoticNetwork/DomoticNetwork/Plugin/PluginLoader.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomoticNetwork.Plugin
+{
+    /// <summary>
+    /// Un singleton encargado de realizar la primera carga de los assemblies (plugins) y de vigilar los eventos sobre la carpeta "\Plugins" y cuando detecta un cambio descargar los plugins y los cargalos de nuevo. Esta clase mantiene una instancia de PluginsContainer.
+    /// </summary>
+    class PluginLoader
+    {
+
+        public List<IPlugin> Modules { get; private set; }
+        public const String DIRECTORY_PLUGIN = "//Plugins";
+        public const String EXTENSION = ".dll";
+
+
+        public PluginLoader()
+        {
+            Modules = new List<IPlugin>();
+
+            //recorro cada fichero del directorio en el que se encuentra esta aplicacion e intento cargarlo como módulo.
+            foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + DIRECTORY_PLUGIN))
+            {
+                LoadPlugin(file);
+            }
+        }
+
+        public void LoadPlugin(string file)
+        {
+            Assembly moduleAssembly;
+
+            if (IsPluginExtension(file))
+            {
+                try
+                {
+                    moduleAssembly = Assembly.LoadFrom(file);
+
+                    foreach (Type moduleType in moduleAssembly.GetTypes())
+                    {
+                        //Se trata de una clase publica, no abstacta.
+                        if (moduleType.IsPublic && !moduleType.IsAbstract && moduleType.IsClass)
+                        {
+                            //Implementa la interfaz IModule.
+                            if (moduleType.GetInterface(typeof(IPlugin).FullName, true) != null)
+                            {
+                                IPlugin i = (IPlugin)Activator.CreateInstance(moduleAssembly.GetType(moduleType.ToString()));
+                                Modules.Add(i);
+                                //break;
+                            }
+                        }
+                    }
+
+                }
+                catch (Exception)
+                { }
+                finally
+                { moduleAssembly = null; }
+            }
+        }
+
+        public bool IsPluginExtension(string file)
+        {
+            return file.EndsWith(EXTENSION);
+        }
+    }
+}
---
+++ DomoticNetwork/DomoticNetwork/Plugin/PluginsContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomoticNetwork.Plugin
+{
+    /// <summary>
+    /// Es una colección de PluginControllers que se encarga de mantener la lista de plugins cargados, registrar y desregistrar/descargar plugins.  Este es el encargado de recorrer la lista de plugin controllers y pedirles que invoquen a los plugins.
+    /// </summary>
+    class PluginsContainer
+    {
+    }
+}
