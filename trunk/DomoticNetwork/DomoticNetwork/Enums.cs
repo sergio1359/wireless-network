83322f736d62e2bc355c3125464938be9614e60c
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-02-23 00:50:29 +0000

    More Updates and more and more and this day never ends :(

--- trunk/DomoticNetwork/DomoticNetwork/Enums.cs
+++ trunk/DomoticNetwork/DomoticNetwork/Enums.cs
@@ -4,5 +4,7 @@ namespace DomoticNetwork
     public static class Enums
     {
         public enum ConnectorType { Dimmer, SwitchHI, SwitchLOW, PWMTTL, IODigital, Analog };
+
+        public enum OPCodeType : byte { Example = 0xFF }
     }
 }
