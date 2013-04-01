ce58bd19ae8f674e8abacd0b7be4669701012d1b
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-04-01 12:18:39 +0000

    Nuevas clases, nuevo diagrama. Implementando los productos y empezando la generacion de la EEPROM

---
+++ SmartHome/Connexion/Products/PinPort.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions.Enums;
+
+namespace Connexion.Products
+{
+    class PinPort
+    {
+        //Direction
+        public Char Port { set; get; }
+        public Byte Pin { set; get; }
+
+        public PinPort(Char Port, Byte Pin)
+        {
+            this.Port = Port;
+            this.Pin = Pin;
+        }
+
+        public PinPort(String direction)
+        {
+            Port = direction[0];
+            Pin = Byte.Parse(direction[1].ToString());
+        }
+
+        public static PinPortConfiguration GetPinPortConfiguration(Enums.HomeDeviceType HomeDeviceType)
+        {
+            switch (HomeDeviceType)
+	        {
+		        case Enums.HomeDeviceType.Buttom:
+                 break;
+                case Enums.HomeDeviceType.Dimmable:
+                 break;
+                case Enums.HomeDeviceType.DoorLock:
+                 break;
+                case Enums.HomeDeviceType.HumiditySensor:
+                 break;
+                case Enums.HomeDeviceType.LightSensor:
+                 break;
+                case Enums.HomeDeviceType.PowerSensor:
+                 break;
+                case Enums.HomeDeviceType.PresenceSensor:
+                 break;
+                case Enums.HomeDeviceType.RGBLight:
+                 break;
+                case Enums.HomeDeviceType.Switch:
+                 break;
+                case Enums.HomeDeviceType.TemperatureSensor:
+                 break;
+                case Enums.HomeDeviceType.WallPlug:
+                 break;
+                default:
+                 break;
+	        }
+        }
+    }
+
+    class PinPortConfiguration 
+    {
+
+        public const bool DEFAULT_OUTPUT = false; //Entrada
+        public const bool DEFAULT_DIGITAL = true; //Digital
+
+
+        public Boolean Output { get; set; }
+        public Boolean Digital { get; set; }
+
+        public enum Trigger : byte { None = 0x00, FallingEdge = 0x01, RisingEdge = 0x10, Both = 0x11 }
+
+        //Digital-------------------------------------
+        //output
+        public Boolean DefaultValueD { get; set; }
+
+        //input
+        public Trigger changeTypeD { get; set; }
+
+
+        //Analog-------------------------------------
+        //output
+        public Byte DefaultValueA { get; set; }
+
+        //input
+        public Byte Increment { get; set; }
+        public Byte Threshold { get; set; }
+    }
+}
---
+++ SmartHome/Connexion/Products/Shield.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Extensions.Enums;
+
+namespace Connexion.Products
+{
+    class Shield
+    {
+
+        public Enums.ShieldType ShieldType { set; get; }
+        public Dictionary<string, List<PinPort>> PinPorts { set; get; }
+
+        public Shield(Enums.ShieldType shieldtype)
+        {
+            ShieldType = shieldtype;
+            
+            switch (shieldtype)
+            {
+                case ShieldType.Example:
+                    AddConnector("Digital0", Enums.ConnectorType.IODigital, new String[] { "A0" });
+                    AddConnector("Digital1", Enums.ConnectorType.IODigital, new String[] { "A1" });
+                    AddConnector("Analog0", Enums.ConnectorType.Analog, new String[] { "F0" });
+                    AddConnector("Analog1", Enums.ConnectorType.Analog, new String[] { "F1" });
+                    AddConnector("Analog2", Enums.ConnectorType.Analog, new String[] { "F2" });
+                    AddConnector("PWM", Enums.ConnectorType.IODigital, new String[] { "B4", "B7", "G5" });
+                    AddConnector("Dimmer0", Enums.ConnectorType.Dimmer, new String[] { "G0" });
+                default:
+                    throw new Exception();
+            }
+        }
+
+
+
+        public void AddConnector(String name, Enums.ConnectorType type, String[] directions)
+        {
+            foreach (String dir in directions)
+            {
+                if (!ShieldBase.IsConnectable(dir, type))
+                    throw new Exception("El tipo de conector no es compatible en esa direccion del micro");
+            }
+
+            Connectors.Add(new Connector(name, type, directions));
+        }
+
+        public Connector GetConector(Char port, Byte pin)
+        {
+            return Connectors.FirstOrDefault<Connector>(x => x.Directions.Exists(y => y.Pin == pin && y.Port == port));
+        }
+
+        public Connector GetConector(Byte port, Byte pin)
+        {
+            return Connectors.FirstOrDefault<Connector>(x => x.Directions.Exists(y => y.Pin == pin && y.Port - 'A' == port));
+        }
+
+        public PinPort GetPinPort(Char port, Byte pin)
+        {
+            if (GetConector(port, pin) == null)
+                return null;
+            else
+                return GetConector(port, pin).Directions.FirstOrDefault<PinPort>(x => x.Port == port && x.Pin == pin);
+        }
+
+        public PinPort GetPinPort(Byte port, Byte pin)
+        {
+            if (GetConector(port, pin) == null)
+                return null;
+            else
+                return GetConector(port, pin).Directions.FirstOrDefault<PinPort>(x => x.Port - 'A' == port && x.Pin == pin);
+        }
+    }    
+}
