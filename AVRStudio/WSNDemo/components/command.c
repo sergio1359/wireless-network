492475595114111339b55b89eb89bfae9a00d094
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-05-03 09:01:18 +0000

    - Some files added (schematic for charger & bill of materials Basic_Board.xls)

--- AVRStudio/WSNDemo/components/command.c
+++ AVRStudio/WSNDemo/components/command.c
@@ -10,24 +10,77 @@ uint8_t getCommandArgsLenght(uint8_t* opcode)
 {
 	switch (*opcode)
 	{
-		case DIGITAL_READ_OPCODE:
-			return DIGITAL_READ_LENGHT;
-			break;
+		case DigitalWrite:
+		return sizeof(DIGITAL_WRITE_MESSAGE_t);
 		
-		case DIGITAL_WRITE_OPCODE:
-			return DIGITAL_WRITE_LENGHT;
-			break;
+		case DigitalSwitch:
+		return sizeof(DIGITAL_SWITCH_MESSAGE_t);
 		
-		case DIGITAL_SWITCH_OPCODE:
-			return DIGITAL_SWITCH_LENGHT;
-			break;
+		case  DigitalReadResponse:
+		return sizeof(DIGITAL_READ_RESPONSE_MESSAGE_t);
 		
-		case 0xFF: //REPRODUCIR BACHATA
-			return 4;
-			break;
+		case DigitalRead:
+		return sizeof(DIGITAL_READ_MESSAGE_t);
+		
+		
+		
+		case AnalogWrite:
+		return sizeof(ANALOG_WRITE_MESSAGE_t);
+
+		case AnalogRead:
+		return sizeof(ANALOG_READ_MESSAGE_t);
+		
+		case AnalogReadResponse:
+		return sizeof(ANALOG_READ_RESPONSE_MESSAGE_t);
+		
+		
+		
+		case Reset:
+		return sizeof(RESET_MESSAGE_t);
+		
+		case RouteTableRead:
+		return sizeof(ROUTE_TABLE_READ_t);
+		
+		case RouteTableReadResponse:
+		return sizeof(ROUTE_TABLE_READ_RESPONSE_HEADER_t) + *(opcode+1); //CHECK!!!!!!!!! LENGHT READ
+		
+		
+		
+		case ConfigWrite:
+		return sizeof(CONFIG_WRITE_HEADER_MESSAGE_t) + *(opcode+1); //CHECK!!!!!!!!! LENGHT READ
+		
+		case ConfigRead:
+		return sizeof(CONFIG_READ_MESSAGE_t);
+		
+		case ConfigReadResponse:
+		return sizeof(CONFIG_READ_RESPONSE_HEADER_MESSAGE_t) + *(opcode+1); //CHECK!!!!!!!!! LENGHT READ
+		
+		case ConfigChecksum:
+		return sizeof(CONFIG_CHECKSUM_MESSAGE_t);
+		
+		case ConfigChecksumResponse:
+		return sizeof(CONFIG_CHECKSUM_RESPONSE_MESSAGE_t);
+		
+		
+		
+		case TimeWrite:
+		return sizeof(TIME_WRITE_MESSAGE_t);
+		
+		case TimeRead:
+		return sizeof(TIME_READ_MESSAGE_t);
+		
+		case TimeReadResponse:
+		return sizeof(TIME_READ_RESPONSE_MESSAGE_t);
+		
+		
+		
+		case  0xFF:
+		return 4;// JUST FOR FUN!
+		
+		//case Extension:
+		//return getCommandArgsLenght(opcode+1);
 		
 		default:
-			return 0xFF;
-			break;
+		return 0xFF;
 	}
 }
--- AVRStudio/WSNDemo/components/inc/command.h
+++ AVRStudio/WSNDemo/components/inc/command.h
@@ -10,15 +10,170 @@
 #define COMMAND_H_
 
 #include <stdint.h>
+#include "RTC.h"
 
-#define DIGITAL_READ_OPCODE 0x01
-#define DIGITAL_READ_LENGHT 1
+typedef enum COMMAND_OPCODES
+{
+	Reset = 0x00,
+	RouteTableRead = 0x01,
+	RouteTableReadResponse = 0x02,
 
-#define DIGITAL_WRITE_OPCODE 0x02
-#define DIGITAL_WRITE_LENGHT 3
+	DigitalWrite = 0x05,
+	DigitalSwitch = 0x06,
+	DigitalRead = 0x07,
+	DigitalReadResponse = 0x08,
 
-#define DIGITAL_SWITCH_OPCODE 0x03
-#define DIGITAL_SWITCH_LENGHT 2
+	AnalogWrite = 0x10,
+	AnalogRead = 0x11,
+	AnalogReadResponse = 0x12,
+
+	TimeWrite = 0x20,
+	TimeRead = 0x21,
+	TimeReadResponse = 0x22,
+
+	Warning = 0x30,
+	Error = 0x31,
+
+	ConfigWrite = 0x40,
+	ConfigRead = 0x41,
+	ConfigReadResponse = 0x42,
+	ConfigChecksum = 0x43,
+	ConfigChecksumResponse = 0x44,
+
+	ColorWrite = 0x50,
+	ColorWriteRandom = 0x51,
+	ColorSecuenceWrite = 0x52,
+	ColorSortedSecuenceWrite = 0x53,
+	ColorRead = 0x54,
+	ColorReadResponse = 0x55,
+
+	PresenceRead = 0x57,
+	PresenceReadResponse = 0x58,
+
+	TemperatureRead = 0x5A,
+	TemperatureReadResponse = 0x5B,
+	HumidityRead = 0x5C,
+	HumidityReadResponse = 0x5D,
+
+	PowerRead = 0x60,
+	PowerReadResponse = 0x61,
+
+	LuminosityRead = 0x63,
+	LuminosityReadResponse = 0x64,
+
+	Extension = 0xFF,
+}COMMAND_OPCODES;
+
+//DIGITAL PORT MESSAGES
+typedef struct
+{
+	uint8_t dir;
+	uint8_t mask;
+	uint8_t value;
+	TIME_t time;
+}DIGITAL_WRITE_MESSAGE_t;
+
+typedef struct
+{
+	uint8_t dir;
+	uint8_t mask;
+	TIME_t time;
+}DIGITAL_SWITCH_MESSAGE_t;
+
+typedef struct
+{
+	uint8_t dir;
+}DIGITAL_READ_MESSAGE_t;
+
+typedef struct
+{
+	uint8_t dir;
+	uint8_t value;
+}DIGITAL_READ_RESPONSE_MESSAGE_t;
+
+
+
+//ANALOG PORT MESSAGES
+typedef struct
+{
+	uint8_t dir;
+	uint8_t value;
+	TIME_t time;
+}ANALOG_WRITE_MESSAGE_t;
+
+typedef struct
+{
+	uint8_t dir;
+}ANALOG_READ_MESSAGE_t;
+
+typedef struct
+{
+	uint8_t dir;
+	uint8_t value;
+}ANALOG_READ_RESPONSE_MESSAGE_t;
+
+
+
+// CONTROL
+typedef struct
+{
+}RESET_MESSAGE_t;
+
+typedef struct
+{
+}ROUTE_TABLE_READ_t;
+
+typedef struct
+{
+	uint16_t length;
+}ROUTE_TABLE_READ_RESPONSE_HEADER_t;
+
+
+
+// CONFIGURATION
+typedef struct
+{
+	uint16_t length;
+}CONFIG_WRITE_HEADER_MESSAGE_t;
+
+typedef struct
+{
+}CONFIG_READ_MESSAGE_t;
+
+typedef struct
+{
+	uint16_t length;
+}CONFIG_READ_RESPONSE_HEADER_MESSAGE_t;
+
+typedef struct
+{
+}CONFIG_CHECKSUM_MESSAGE_t;
+
+typedef struct
+{
+	uint16_t checksum;
+}CONFIG_CHECKSUM_RESPONSE_MESSAGE_t;
+
+
+
+//TIEMPO
+typedef struct
+{
+	TIME_t time;
+}TIME_WRITE_MESSAGE_t;
+
+typedef struct
+{
+}TIME_READ_MESSAGE_t;
+
+typedef struct
+{
+	TIME_t time;
+}TIME_READ_RESPONSE_MESSAGE_t;
+
+
+
+
 
 uint8_t getCommandArgsLenght(uint8_t* opcode);
 
