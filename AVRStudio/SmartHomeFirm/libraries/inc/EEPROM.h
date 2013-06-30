3cd08dd91a17456e4712ed3a5095694587d3b0cb
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-06-30 18:53:51 +0000

    -Eeprom structs updated

--- AVRStudio/SmartHomeFirm/libraries/inc/EEPROM.h
+++ AVRStudio/SmartHomeFirm/libraries/inc/EEPROM.h
@@ -14,19 +14,16 @@
 #include "config.h"
 
 
-#define OPERATION_INDEX_START_ADDR						runningConfiguration.topConfiguration.dinamicIndex.portOperationIndex
-#define OPERATION_LIST_START_ADDR						runningConfiguration.topConfiguration.dinamicIndex.portOperationIndex
-
-#define OPERATION_RESTRIC_LIST_START_ADDRESS			runningConfiguration.topConfiguration.dinamicIndex.portOperationTimeRestrictionList
 #define TIME_OPERATION_LIST_START_ADDRESS				runningConfiguration.topConfiguration.dinamicIndex.timeOperationList
-#define MODULE_RGB_CONFIG_ADDRESS						runningConfiguration.topConfiguration.dinamicIndex.configModule_RGB
-#define MODULE_PRESENCE_CONFIG_ADDRESS					runningConfiguration.topConfiguration.dinamicIndex.configModule_Presence
+#define OPERATION_LIST_START_ADDR						runningConfiguration.topConfiguration.dinamicIndex.operationList
+#define OPERATION_TIME_RESTRIC_LIST_START_ADDRESS		runningConfiguration.topConfiguration.dinamicIndex.operationTimeRestrictionList
+#define OPERATION_COND_RESTRIC_LIST_START_ADDRESS		runningConfiguration.topConfiguration.dinamicIndex.operationConditionRestrictionList
 #define FREE_REGION_START_ADDRESS						runningConfiguration.topConfiguration.dinamicIndex.freeRegion
 
-#define OPERATION_INDEX_END_ADDR						OPERATION_LIST_START_ADDR						//Renowned for greater understanding
-#define OPERATION_LIST_END_ADDR							OPERATION_RESTRIC_LIST_START_ADDRESS
-#define OPERATION_RESTRIC_LIST_END_ADDRESS				TIME_OPERATION_LIST_START_ADDRESS				
-#define TIME_OPERATION_LIST_END_ADDRESS					MODULE_RGB_CONFIG_ADDRESS		
+#define TIME_OPERATION_LIST_END_ADDRESS					OPERATION_LIST_START_ADDR
+#define OPERATION_LIST_END_ADDR							OPERATION_TIME_RESTRIC_LIST_START_ADDRESS
+#define OPERATION_TIME_RESTRIC_LIST_END_ADDRESS			OPERATION_COND_RESTRIC_LIST_START_ADDRESS						
+#define OPERATION_COND_RESTRIC_LIST_END_ADDRESS			FREE_REGION_START_ADDRESS
 
 #define NO_EDGE			0
 #define FALLING_EDGE	1
@@ -34,69 +31,41 @@
 #define BOTH_EDGE		3
 
 typedef struct{
-	unsigned LedDebug : 1; //LSB
+	unsigned ledDebug : 1; //LSB
 	unsigned UARTDebug : 1;
-	unsigned BatteryInstalled : 1;
-	unsigned Reserved : 5; //MSB
+	unsigned batteryInstalled : 1;
+	unsigned reserved : 5; //MSB
 }SYSTEM_FLAGS_t;
 
 typedef struct{
-	uint8_t baseModel;  
-	uint16_t length; //Size of the current config
-	uint16_t checkSum; //crc16
+	uint8_t firmVersion;	//Desired firmware version for the current config
+	uint16_t length;		//Size of the current config
+	uint16_t checkSum;		//Crc16
 	DATE_t updateDate;
 	TIME_t updateTime;
 	SYSTEM_FLAGS_t systemFlags;
-	uint8_t networkRetriesLimit;
-}DEVICE_INFO_t;
+}CONFIG_HEADER_t;
 
 typedef struct{
 	uint16_t deviceAddress;
 	uint8_t channel;
 	uint16_t panId;
 	uint8_t securityKey[16];
+	uint8_t networkRetries;
 }NETWORK_CONFIG_t;
 
-typedef struct{
-	unsigned pin0_ChangeType : 2;	//Two for pin 00-> noEdge	10-> risingEdge		01-> fallingEdge	11->both
-	unsigned pin1_ChangeType : 2;
-	unsigned pin2_ChangeType : 2;
-	unsigned pin3_ChangeType : 2;
-	unsigned pin4_ChangeType : 2;
-	unsigned pin5_ChangeType : 2;
-	unsigned pin6_ChangeType : 2;
-	unsigned pin7_ChangeType : 2;
-}DIGITAL_PORT_CHANGE_TYPE_t;
 
 typedef struct{
-	//uint8_t maskDIGITAL; //0-> disabled		1-> enabled
-	uint8_t maskIO;   //0-> inputs		1-> outputs
-	uint8_t maskAD;   //0-> analog		1-> digital													//TOBEREMOVED
-	uint8_t defaultValuesD;		 //Initial values DIGITAL
-	uint16_t changeTypeD; //Two for pin	10-> risingEdge		01-> fallingEdge	11->both			//TOBEREMOVED
-}PORT_CONFIG_t;
-
-typedef struct{
-	//uint8_t maskPWM; //0-> disabled		1-> enabled
-	uint8_t defaultValueA;   //Initial value ANALOG output (PWM)
-}PWM_CONFIG_t;
-
-typedef struct{
-	//uint8_t maskANALOG; //0-> disabled		1-> enabled
-	uint8_t increment;   //Difference between the last and current value
-	uint8_t threshold;	 //hysteresis value to exceed
-}ANALOG_EVENT_CONFIG_t;
-
-typedef struct{
-	uint16_t portOperationIndex;
-	uint16_t portOperationList;
-	uint16_t portOperationTimeRestrictionList;
-	uint16_t timeOperationList;
+	uint16_t configModule_Logic;
 	uint16_t configModule_RGB;
 	uint16_t configModule_Presence;
 	uint16_t configModule_TempHum;
-	uint16_t configModule_Power;
-	uint16_t configModule_LightSensor;
+	uint16_t configModule_EnergyMeter;
+	uint16_t configModule_Luminosity;
+	uint16_t timeOperationList;
+	uint16_t operationList;
+	uint16_t operationTimeRestrictionList;
+	uint16_t operationConditionRestrictionList;
 	uint16_t freeRegion;
 }DINAMIC_INDEX_t;
 
@@ -107,14 +76,14 @@ typedef struct{
 }OPERATION_HEADER_t;
 
 typedef struct{
-	WEEKDAY_t weekDays;
 	TIME_t activationTime;				  //Activation time
 	OPERATION_HEADER_t operationHeader;   //Header of the operation to send on activation
 }TIME_OPERATION_HEADER_t;
 
-typedef struct
-{
+typedef struct{
 	uint16_t operationAddress;
+	DATE_t dateFrom;
+	DATE_t dateTo;
 	WEEKDAY_t weekDays;
 	TIME_t start;
 	TIME_t end;
@@ -122,31 +91,8 @@ typedef struct
 
 
 typedef struct {
-	DEVICE_INFO_t deviceInfo;
+	CONFIG_HEADER_t configHeader;
 	NETWORK_CONFIG_t networkConfig;
-	PORT_CONFIG_t portConfig_PA;
-	PORT_CONFIG_t portConfig_PB;
-	PORT_CONFIG_t portConfig_PC;
-	PORT_CONFIG_t portConfig_PD;
-	PORT_CONFIG_t portConfig_PE;
-	PORT_CONFIG_t portConfig_PF;
-	PORT_CONFIG_t portConfig_PG;
-	PWM_CONFIG_t pwmConfig_PWM0;
-	PWM_CONFIG_t pwmConfig_PWM1;
-	PWM_CONFIG_t pwmConfig_PWM2;
-	PWM_CONFIG_t pwmConfig_PWM3;
-	PWM_CONFIG_t pwmConfig_PWM4;
-	PWM_CONFIG_t pwmConfig_PWM5;
-	PWM_CONFIG_t pwmConfig_PWM6;
-	PWM_CONFIG_t pwmConfig_PWM7;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC0;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC1;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC2;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC3;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC4;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC5;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC6;
-	ANALOG_EVENT_CONFIG_t analogConfig_ADC7;
 	DINAMIC_INDEX_t dinamicIndex;
 }TOP_CONFIGURATION_t;
 
