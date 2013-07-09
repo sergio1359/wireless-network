477432c4405ab5f267371bd25ebc40c9ef8943e4
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-07-09 18:23:32 +0000

    TimeReadResponse implemented Now WeekDay_t and Date_t are in different structures

--- trunk/AVRStudio/SmartHomeFirm/modules/time_module.c
+++ trunk/AVRStudio/SmartHomeFirm/modules/time_module.c
@@ -16,24 +16,10 @@ struct
 	TIME_READ_RESPONSE_MESSAGE_t response;
 }timeResponse;
 
-struct
-{
-	OPERATION_HEADER_t header;
-	DATE_READ_RESPONSE_MESSAGE_t response;
-}dateResponse;
-
-struct
-{
-	OPERATION_HEADER_t header;
-	DATE_TIME_REQUEST_RESPONSE_MESSAGE_t response;
-}datimeRequestResponse;
-
 
 void timeModule_Init(void)
 {
-	timeResponse.header.opCode = TimeReadResponse;
-	dateResponse.header.opCode = DateReadResponse;
-	datimeRequestResponse.header.opCode = DateTimeRequestResponse;
+	timeResponse.header.opCode = DateTimeReadResponse;
 }
 
 void timeModule_NotificationInd(uint8_t sender, OPERATION_HEADER_t* notification)
@@ -43,64 +29,33 @@ void timeModule_NotificationInd(uint8_t sender, OPERATION_HEADER_t* notification
 
 void time_Handler(OPERATION_HEADER_t* operation_header)
 {
-	if(operation_header->opCode == TimeWrite)
+	if(operation_header->opCode == DateTimeWrite)
 	{
 		TIME_WRITE_MESSAGE_t* msg = (TIME_WRITE_MESSAGE_t*)(operation_header + 1);
 		TIME_ValidateTime(&msg->time);
+		TIME_ValidateDate(&msg->date, &msg->week);
 		
-	}else if(operation_header->opCode == TimeRead)
+	}else if(operation_header->opCode == DateTimeRead)
 	{
 		timeResponse.header.destinationAddress = operation_header->sourceAddress;
 		timeResponse.header.sourceAddress = runningConfiguration.topConfiguration.networkConfig.deviceAddress;
-		timeResponse.response.time = currentTime;
 		
-		OM_ProccessResponseOperation(&timeResponse.header);
-	}else if(operation_header->opCode == TimeReadResponse)
-	{
-		//TODO: NOTIFICATION
-	}
-}
-
-void date_Handler(OPERATION_HEADER_t* operation_header)
-{
-	if(operation_header->opCode == DateWrite)
-	{
-		DATE_WRITE_MESSAGE_t* msg = (DATE_WRITE_MESSAGE_t*)(operation_header + 1);
-		TIME_ValidateDate(&msg->date);
-	}else if(operation_header->opCode == DateRead)
-	{
-		dateResponse.header.destinationAddress = operation_header->sourceAddress;
-		dateResponse.header.sourceAddress = runningConfiguration.topConfiguration.networkConfig.deviceAddress;
-		dateResponse.response.date = currentDate;
-		
-		OM_ProccessResponseOperation(&dateResponse.header);
-	}else if(operation_header->opCode == DateReadResponse)
-	{
-		//TODO: NOTIFICATION
-	}
-}
-
-void request_Handler(OPERATION_HEADER_t* operation_header)
-{
-	if(operation_header->opCode == DateTimeRequest)
-	{
-		//Only answer if a valid Date & Time is stored
 		if(VALID_DATETIME)
 		{
-			datimeRequestResponse.header.destinationAddress = operation_header->sourceAddress;
-			datimeRequestResponse.header.sourceAddress = runningConfiguration.topConfiguration.networkConfig.deviceAddress;
-			datimeRequestResponse.response.date = currentDate;
-			datimeRequestResponse.response.time = currentTime;
-			
-			OM_ProccessResponseOperation(&datimeRequestResponse.header);	
-		}
-	}else if(operation_header->opCode == DateTimeRequestResponse)
+			timeResponse.response.week = currentWeek;
+			timeResponse.response.date = currentDate;
+			timeResponse.response.time = currentTime;
+		}else
+		{
+			for(uint8_t i = 0; i < sizeof(TIME_READ_RESPONSE_MESSAGE_t); i++)
+			{
+				((uint8_t*)&timeResponse.response)[i] = 0xFF;
+			}
+		}		
+		
+		OM_ProccessResponseOperation(&timeResponse.header);
+	}else if(operation_header->opCode == DateTimeReadResponse)
 	{
-		DATE_TIME_REQUEST_RESPONSE_MESSAGE_t* msg = (DATE_TIME_REQUEST_RESPONSE_MESSAGE_t*)(operation_header + 1);
-		
-		TIME_ValidateDate(&msg->date);
-		TIME_ValidateTime(&msg->time);
-		
 		//TODO: NOTIFICATION
 	}
 }
