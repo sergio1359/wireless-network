ccbf22badbe73bbd48181b0dcce0dd275e06ad50
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-07-26 21:26:12 +0000

    SerialPortManager Project Example added

---
+++ SerialPortManager/SerialPortManager/ConnectionManager/EventHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPortManager.ConnectionManager
+{
+    public static class EventHelper
+    {
+        public static void RaiseEventOnUIThread(Delegate theEvent, object[] args)
+        {
+            foreach (Delegate d in theEvent.GetInvocationList())
+            {
+                ISynchronizeInvoke syncer = d.Target as ISynchronizeInvoke;
+                if (syncer == null)
+                {
+                    d.DynamicInvoke(args);
+                }
+                else
+                {
+                    syncer.BeginInvoke(d, args);  // cleanup omitted
+                }
+            }
+        }
+    }
+}
