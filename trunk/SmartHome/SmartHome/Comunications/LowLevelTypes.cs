5c0a18f543ce013a794115b89431ede6f5501625
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-07-31 21:02:07 +0000

    - Low Level methods implementation.

---
+++ trunk/SmartHome/SmartHome/Comunications/LowLevelTypes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome.Comunications
+{
+    public enum WeekDays : byte
+    {
+        Monday = 0x40,
+        Tuesday = 0x20,
+        Wednesday = 0x10,
+        Thursday = 0x08,
+        Friday = 0x04,
+        Saturday = 0x2,
+        Sunday = 0x01,
+    }
+
+    public enum LogicWriteValues : byte
+    {
+        Clear = 0x00,
+        Set = 0x01,
+        SetRelative = 0x80,
+        SetRelativeInverse = 0x81
+    }
+}
