c392c2d1035d518c0da1ad1ef1eeb937f2f496cf
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-08-04 12:15:23 +0000

    - APP_SESION structs implemented

--- AVRStudio/SmartHomeFirm/modules/inc/config_module.h
+++ AVRStudio/SmartHomeFirm/modules/inc/config_module.h
@@ -23,11 +23,11 @@ X(ShieldModelRead,				0x03, configShield_Handler,		0x00,						SHIELD_MODEL_READ_
 X(ShieldModelReadResponse,		0x04, configShield_Handler,		0x00,						SHIELD_MODEL_READ_RESPONSE_MESSAGE_t,		false)	\
 X(BaseModelRead,				0x05, configBaseModel_Handler,	0x00,						BASE_MODEL_READ_MESSAGE_t,					false)	\
 X(BaseModelReadResponse,		0x06, configBaseModel_Handler,	0x00,						BASE_MODEL_READ_RESPONSE_MESSAGE_t,			false)	\
-X(ConfigWrite,					0x07, configWrite_Handler,		configWrite_DataConf,		CONFIG_WRITE_HEADER_MESSAGE_t,				true)	\
+X(ConfigWrite,					0x07, configWrite_Handler,		0x00,						CONFIG_WRITE_HEADER_MESSAGE_t,				true)	\
 X(ConfigWriteResponse,			0x08, configWrite_Handler,		configWrite_DataConf,		CONFIG_WRITE_RESPONSE_MESSAGE_t,			false)	\
-X(ConfigRead,					0x09, configRead_Handler,		configRead_DataConf,		CONFIG_READ_MESSAGE_t,						false)	\
+X(ConfigRead,					0x09, configRead_Handler,		0x00,						CONFIG_READ_MESSAGE_t,						false)	\
 X(ConfigReadResponse,			0x0A, configRead_Handler,		configRead_DataConf,		CONFIG_READ_RESPONSE_HEADER_MESSAGE_t,		true)	\
-X(ConfigReadConfirmation,		0x0B, configRead_Handler,		configRead_DataConf,		CONFIG_READ_CONFIRMATION_MESSAGE_t,			false)	\
+X(ConfigReadConfirmation,		0x0B, configRead_Handler,		0x00,						CONFIG_READ_CONFIRMATION_MESSAGE_t,			false)	\
 X(ConfigChecksum,				0x0C, configChecksum_Handler,	0x00,						CONFIG_CHECKSUM_MESSAGE_t,					false)	\
 X(ConfigChecksumResponse,		0x0D, configChecksum_Handler,	0x00,						CONFIG_CHECKSUM_RESPONSE_MESSAGE_t,			false)	\
 
