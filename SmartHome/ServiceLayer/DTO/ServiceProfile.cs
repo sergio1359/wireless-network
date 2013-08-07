a011c07f0a960ad201e81c061abb5f3ec4e017fb
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-08-07 21:27:45 +0000

    - AutoMapper

---
+++ SmartHome/ServiceLayer/DTO/ServiceProfile.cs
@@ -0,0 +1,35 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using SmartHome.Network; 
+#endregion
+
+namespace ServiceLayer.DTO
+{
+    public class ServiceProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "ServiceProfile"; }
+        }
+
+        protected override void Configure()
+        {
+            Map();
+        }
+
+        private void Map()
+        {
+            Mapper.CreateMap<Connector, ConnectorDTO>()
+                // t: Destination entity (ConnectorDTO).
+                // f: Source entity (Connector).
+                // r: Source entity (Connector).
+                //.ForMember(t => t.Name, f => f.MapFrom(r => r.Name))
+                .ReverseMap();
+        }
+    }
+}
