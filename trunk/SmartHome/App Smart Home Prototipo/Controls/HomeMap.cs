cba21d042796e5d0f1b4843ca8160ecbdc692866
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-08-09 16:57:55 +0000

    Commit necesario para hacer una pota en el visual studio

---
+++ trunk/SmartHome/App Smart Home Prototipo/Controls/HomeMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Smart_Home_Prototipo.Controls
+{
+    public partial class HomeMap : UserControl
+    {
+        public HomeMap()
+        {
+            InitializeComponent();
+        }
+    }
+}
