fc1a23208feb7441f1ce2f683003d9d45f860105
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-08-22 19:23:28 +0000

    - UnitOfWork implementation

---
+++ trunk/SmartHome/DataLayer/UnitOfWork.cs
@@ -0,0 +1,159 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+#endregion
+
+namespace DataLayer
+{
+    public class UnitOfWork : IDisposable
+    {
+        #region Private Fields
+        private SmartHomeDBContext context;
+
+        private NodeRepository nodeRepository;
+        private HomeRepository homeRepository;
+        private ThemesRepository themesRespository;
+        private TimeOperationRepository timeOperationRepository;
+        private ZoneRepository zoneRepository;
+        private HomeDeviceRepository homeDeviceRepository;
+        private ViewRepository viewRepository;
+        private LogRepository logRepository;
+        private ConnectorRepository connectorRepository;
+        private OperationRepository operationRepository;
+        private LocationRepository locationRepository; 
+        #endregion
+
+        #region Properties
+        public NodeRepository NodeRespository
+        {
+            get
+            {
+                if (nodeRepository == null)
+                    nodeRepository = new NodeRepository(context);
+
+                return nodeRepository;
+            }
+        }
+
+        public LocationRepository LocationRepository
+        {
+            get
+            {
+                if (locationRepository == null)
+                    locationRepository = new LocationRepository(context);
+                return locationRepository;
+            }
+        }
+
+        public HomeDeviceRepository HomeDeviceRespository
+        {
+            get
+            {
+                if (homeDeviceRepository == null)
+                    homeDeviceRepository = new HomeDeviceRepository(context);
+                return homeDeviceRepository;
+            }
+        }
+
+        public HomeRepository HomeRespository
+        {
+            get
+            {
+                if (homeRepository == null)
+                    homeRepository = new HomeRepository(context);
+                return homeRepository;
+            }
+        }
+
+        public ThemesRepository ThemesRespository
+        {
+            get
+            {
+                if (themesRespository == null)
+                    themesRespository = new ThemesRepository(context);
+                return themesRespository;
+            }
+        }
+
+        public TimeOperationRepository TimeOperationRepository
+        {
+            get
+            {
+                if (timeOperationRepository == null)
+                    timeOperationRepository = new TimeOperationRepository(context);
+                return timeOperationRepository;
+            }
+        }
+
+        public ZoneRepository ZoneRepository
+        {
+            get
+            {
+                if (zoneRepository == null)
+                    zoneRepository = new ZoneRepository(context);
+                return zoneRepository;
+            }
+        }
+
+        public ViewRepository ViewRepository
+        {
+            get
+            {
+                if (viewRepository == null)
+                    viewRepository = new ViewRepository(context);
+                return viewRepository;
+            }
+        }
+
+        public LogRepository LogRepository
+        {
+            get
+            {
+                if (logRepository == null)
+                    logRepository = new LogRepository(context);
+                return logRepository;
+            }
+        }
+
+        public ConnectorRepository ConnectorRepository
+        {
+            get
+            {
+                if (connectorRepository == null)
+                    connectorRepository = new ConnectorRepository(context);
+                return connectorRepository;
+            }
+        }
+
+        public OperationRepository OperationRepository
+        {
+            get
+            {
+                if (operationRepository == null)
+                    operationRepository = new OperationRepository(context);
+                return operationRepository;
+            }
+        } 
+        #endregion
+
+        public UnitOfWork()
+        {
+            this.context = new SmartHomeDBContext();
+        }
+
+        #region Public Methods
+        public void Commit()
+        {
+            this.context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        } 
+        #endregion
+    }
+}
