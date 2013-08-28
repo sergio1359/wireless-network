d3d36bdbe374a6dd2425c8cbd7f7253b938d6c3f
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-08-28 16:34:04 +0000

    - ConfigModule using LastChecksumUpdate - RefreshStatus method in UserModule prototype

--- trunk/SmartHome/SmartHome/Communications/Modules/UserModule.cs
+++ trunk/SmartHome/SmartHome/Communications/Modules/UserModule.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataLayer.Entities.HomeDevices;
 using SmartHome.Communications.Modules.Common;
 using SmartHome.Comunications;
 using SmartHome.Comunications.Messages;
@@ -14,6 +15,11 @@ namespace SmartHome.Communications.Modules
 {
     class UserModule : ModuleBase
     {
+        [RequiredModule]
+        public StatusModule statusModule;
+
+        private Dictionary<HomeDevice, TaskCompletionSource<bool>> pendingRequests;
+
         public UserModule(CommunicationManager communicationManager)
             : base(communicationManager)
         {
@@ -21,6 +27,13 @@ namespace SmartHome.Communications.Modules
         }
 
         #region Overridden Methods
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            statusModule.StateRefreshed += statusModule_StateRefreshed;
+        }
+
         public override void ProcessReceivedMessage(Comunications.Messages.IMessage inputMessage)
         {
             //Do Nothing
@@ -47,5 +60,21 @@ namespace SmartHome.Communications.Modules
                 routingEnabled: true);
         } 
         #endregion
+
+        #region Private Methods
+        void statusModule_StateRefreshed(object sender, HomeDevice e)
+        {
+            if (this.pendingRequests.ContainsKey(e))
+            {
+                this.pendingRequests[e].SetResult(true);
+            }
+        }
+        #endregion
+
+        public async Task<bool> RefreshHomeDevice(HomeDevice homeDevice, float timeout)
+        {
+            //TODO: To be implemented
+            return false;
+        }
     }
 }
