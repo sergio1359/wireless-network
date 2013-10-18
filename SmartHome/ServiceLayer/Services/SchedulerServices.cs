c270627f0c82002176f44f422ed7d0c1941220c4
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-10-18 14:41:40 +0000

    - separate scheduler and theme services

---
+++ SmartHome/ServiceLayer/Services/SchedulerServices.cs
@@ -0,0 +1,87 @@
+﻿#region Using Statements
+using AutoMapper;
+using DataLayer;
+using DataLayer.Entities;
+using DataLayer.Entities.HomeDevices;
+using ServiceLayer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace ServiceLayer
+{
+    public class SchedulerServices
+    {
+        public IEnumerable<TimeOperationDTO> GetScheduler()
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                IEnumerable<TimeOperation> timeOps;
+
+                timeOps = repository.TimeOperationRepository.GetAll();
+
+                return Mapper.Map<IEnumerable<TimeOperationDTO>>(timeOps);
+            }
+        }
+
+        public int AddScheduler(byte weekDays, TimeSpan time, string name, int idHomeDeviceDestination, string operation, object[] args = null)
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                HomeDevice homeDevice = repository.HomeDeviceRespository.GetById(idHomeDeviceDestination);
+
+                if (homeDevice == null)
+                    return -1;
+
+                Operation operationInternal = new Operation()
+                {
+                    Args = args,
+                    Name = name,
+                    OperationName = operation,
+                    DestionationHomeDevice = homeDevice
+                };
+
+                TimeOperation timeOperation = new TimeOperation()
+                {
+                    MaskWeekDays = weekDays,
+                    Time = time,
+                    Operation = operationInternal
+                };
+                int idRes = repository.TimeOperationRepository.Insert(timeOperation).Id;
+
+                if (homeDevice.InUse)
+                {
+                    //UPDATE CHECKSUM
+                    homeDevice.Connector.Node.UpdateChecksum(null);
+                }
+
+                repository.Commit();
+
+                return idRes;
+            }
+        }
+
+        public void RemoveTimeOperation(int idTimeOperation)
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                TimeOperation timeOp = repository.TimeOperationRepository.GetById(idTimeOperation);
+
+                if (timeOp != null)
+                {
+                    if (timeOp.Operation.DestionationHomeDevice.InUse)
+                    {
+                        //UPDATE CHECKSUM
+                        timeOp.Operation.DestionationHomeDevice.Connector.Node.UpdateChecksum(null);
+                    }
+
+                    repository.TimeOperationRepository.Delete(timeOp);
+                    repository.Commit();
+                }
+            }
+        }
+    }
+}
---
+++ SmartHome/ServiceLayer/Services/ThemeServices.cs
@@ -0,0 +1,71 @@
+﻿#region Using Statements
+using AutoMapper;
+using DataLayer;
+using DataLayer.Entities;
+using ServiceLayer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHome.BusinessEntities;
+#endregion
+
+namespace ServiceLayer
+{
+    public class ThemeServices
+    {
+        public IEnumerable<ThemeDTO> GetThemes()
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                var themes = repository.ThemesRespository.GetAll();
+                return Mapper.Map<IEnumerable<ThemeDTO>>(themes);
+            }
+        }
+
+        public void ExecuteTheme(int idTheme)
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                Theme theme = repository.ThemesRespository.GetById(idTheme);
+
+                if (theme != null)
+                    theme.ExecuteTheme();
+            }
+        }
+
+        public IEnumerable<OperationDTO> GetOperationsOfTheme(int idTheme)
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                Theme theme = repository.ThemesRespository.GetById(idTheme);
+
+                if (theme == null)
+                    return null;
+
+                return Mapper.Map<IEnumerable<OperationDTO>>(theme.Operations);
+            }
+        }
+
+        public void RemoveTheme(int idTheme)
+        {
+            using (UnitOfWork repository = new UnitOfWork())
+            {
+                Theme theme = repository.ThemesRespository.GetById(idTheme);
+
+                if (theme != null)
+                {
+                    foreach (var item in theme.Operations)
+                    {
+                        Services.OperationService.RemoveOperation(item.Id);
+                    }
+
+                    repository.ThemesRespository.Delete(theme);
+
+                    repository.Commit();
+                }
+            }
+        }
+    }
+}
