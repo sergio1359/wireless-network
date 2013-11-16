d87b39f9f5058b07716c29edbb6a43f57f77078c
Elloko2006@gmail.com <Elloko2006@gmail.com@d9709729-b9c4-f871-d834-2b16c2718bd6>
2013-11-16 16:54:50 +0000

    ConfigModule minor bugs fixed Added TransisionState to config transfers

--- SmartHome/SmartHome/Communications/Modules/Configuration/TransactionStatus.cs
+++ SmartHome/SmartHome/Communications/Modules/Configuration/TransactionStatus.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace SmartHome.Communications.Modules.Config
 {
+    public enum TransisionStates
+    {
+        WaitingToStart,
+        InProgress,
+        Completed,
+        Aborted,
+    };
+
     public class TransactionStatus
     {
         public ushort NodeAddress { get; set; }
 
         public float Percentage { get; set; }
+
+        public TransisionStates State { get; set; }
     }
 }
