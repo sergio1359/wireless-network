c8465b08a0968a6d05328dd80167fd739d6e276f
rafa.camas <rafa.camas@d9709729-b9c4-f871-d834-2b16c2718bd6>
2014-01-30 12:26:49 +0000

    - added 2 new projects Portal and WebService examples - update the libs - fix some problems with entity framework

---
+++ SmartHome/WebService/Service/ExampleService.cs
@@ -0,0 +1,23 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebService.Request;
+using WebService.Response;
+
+namespace WebService.Service
+{
+    /// <summary>
+    /// Create your ServiceStack web service implementation.
+    /// </summary>
+    public class HelloService : IService
+    {
+        public object Any(HelloRequest request)
+        {
+            //Looks strange when the name is null so we replace with a generic name.
+            var name = request.Name ?? "SmartHome";
+            return new HelloResponse { Result = "Hello, " + name };
+        }
+    }
+}
